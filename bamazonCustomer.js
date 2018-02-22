76606a926efe2ff1e19f8e5776114f3418ba7208
Vaibhav Tank <vaibhav@Macbook-Pro.local>
2018-02-21 21:52:10 -0800

    working prompts for purchasing items

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -14,7 +14,6 @@ connection.connect(function(err) {
 
   console.log("Connection established on id " + connection.threadId + ".\n");
   printAllItems();
-  todoPrompt();
 });
 
 function printAllItems() {
@@ -26,7 +25,7 @@ function printAllItems() {
         "Item ID: " + res[i].item_id + " || " + "Product Name: " + res[i].product_name + " || " + "Price: " + res[i].price
       );
     }
-    connection.end();
+    todoPrompt();
   })
 }
 
@@ -39,12 +38,47 @@ function todoPrompt() {
       choices: ["Purchase an item", "Exit"]
     })
     .then(function(res) {
-      switch (res) {
+      switch (res.todo) {
         case "Purchase an item":
           purchaseItem();
           break;
         case "Exit":
+          connection.end();
           return;
+      }
+    });
+}
+
+function purchaseItem() {
+  inquirer
+    .prompt([{
+      name: "id",
+      type: "input",
+      message: "What is the ID of the item you'd like to purchase?",
+      validate: function(value) {
+        if ((isNaN(value) === false) && (value >= 0)) {
+          return true;
+        }
+        return false;
+      }
+    },
+    {
+      name: "quantity",
+      type: "input",
+      message: "How many units of the product would you like to purchase?"
+    },
+    {
+      name: "confirm",
+      type: "confirm",
+      message: "Are you sure you'd like to make the purchase?"
+    }
+    ])
+    .then(function(res) {
+      if (res.confirm) {
+        console.log(res.id + "; " + res.quantity);
+      }
+      else {
+        todoPrompt();
       }
     });
 }
